125a7d80832b758bbe9fccec776f9bbc70b495a6
Your Name <your.email@example.com>
2026-06-03 01:25:00 +0200

    now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://YOUR_GITHUB_USERNAME.github.io',
-  // Uncomment the line below if your repository is not named as your-username.github.io
-  // base: '/repository-name',
+  site: 'https://royaassadi1234-code.github.io',
+  base: '/CV',
 });
